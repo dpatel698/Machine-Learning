6721c4cf52f00e0f135d1608b847ed38258c3771
dpatel98 <dpatel0698@gmail.com>
2018-08-25 15:40:24 -0400

    Updated predict function docstring

--- Python Projects/Algorithms/general_learning.py
+++ Python Projects/Algorithms/general_learning.py
@@ -7,5 +7,8 @@ import numpy as np
 def predict(x, theta):
     """Predicts the output (y) given an matrix of x parameters.
     The hypothesis function for the model.
+    :param x:     the feature matrix or test data
+    :param theta: the constant weights for the function
+    :return the predicted values computed by the function (theta' * x)
     """
     return np.dot(x, theta)
