29d695f22058ca2e4511aef2a5d15d0b1d88a0aa
dpatel98 <dpatel0698@gmail.com>
2018-08-31 16:19:47 -0400

    Added function to visualize the polynomial fit on a data set

--- Python Projects/Algorithms/polynomial_regression.py
+++ Python Projects/Algorithms/polynomial_regression.py
@@ -1,6 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from linear_regression import LinearRegression
+from general_learning import predict
+from gradient_descent import batch_gradient_descent
+
+
 # from sklearn.preprocessing import PolynomialFeatures
+
 
 class PolynomialRegression:
     """
@@ -17,59 +23,87 @@ class PolynomialRegression:
     # The constant parameters for the model's hypothesis (Dimensions: n x 1)
     theta = None
 
-
     def __init__(self, linear_model, degree=0):
         """
-        Initializes the linear model and sets polynomial features out of it
+        Initializes the linear model and sets polynomial features out of it.
+
         :param linear_model: linear model to base the polynomial regression off of
         :param degree: the highest degree polynomial feature in the model
         """
-        self.linear_model = LinearRegression(linear_model.x, linear_model.y)
-        self.poly_features = self.generate_polynomial(degree)
+        self.linear_model = linear_model
         self.upper_degree = degree
-        self.linear_model.x = self.generate_polynomial(self.upper_degree)
+        self.poly_features = self.generate_polynomial(self.linear_model.x[:, 1:], degree)
+        self.linear_model.x = self.poly_features
 
-    def generate_polynomial(self, degree):
+    def generate_polynomial(self, x, degree):
         """
-        Generates polynomial features for each column in the training matrix up to the degree specified
+        Generates polynomial features for each column in the training matrix up to the degree specified.
+
+        :param      x: the features to make polynomial values from (should not include constant variable row, theta_0)
         :param degree: the highest degree polynomial feature
         :return: a matrix with polynomial features
         """
-        m = np.size(self.linear_model.x, axis=0)
-        width = np.size(self.linear_model.x[:, 1:], axis=1)
+        m = np.size(x, axis=0)
+        width = np.size(x, axis=1)
         poly_mat = np.ones((m, width * degree))
         # For loop could be vectorized
-        for d in range(1, degree + 1):
-            poly_mat[:, degree - 1 * width:degree * width] = (self.linear_model.x[:, 2:] ** degree)
-        poly_mat = np.concatenate([np.ones((np.size(poly_mat, axis=0), 1)), poly_mat], axis=1)
+        for d in range(0, degree):
+            poly_mat[:, d * width:(d + 1) * width] = (x ** (d + 1))
 
-        return poly_mat
+        return np.concatenate([np.ones((m, 1)), poly_mat], axis=1)
 
     def predict(self, x, theta):
         """
         This function transforms linear features into polynomial features and calculates the predicted values from
-        the regression model using the given theta values
-        :param x: the linear feature matrix (Dimensions: m x n)
-        :param theta: the theta vector      (Dimensions: t x 1)
-        :return: the values predicted by the model for each row of features (Dimensions: t x 1)
+        the regression model using the given theta values.
+
+        :param x:     the linear feature matrix  (Dimensions: m x n)
+        :param theta: the constant weight vector (Dimensions: d x 1)
+        :return: the values predicted by the model for each row of features (Dimensions: d x 1)
         """
         # First generate polynomial values from the x linear features
         m = np.size(x, axis=0)
         width = np.size(x[:, 1:], axis=1)
         x_poly = np.ones((m, width * self.upper_degree))
-        for d in range(1, self.upper_degree + 1):
-            x_poly[:, self.upper_degree - 1 * width:self.upper_degree * width] = (x[:, 1:] ** self.upper_degree)
+        for d in range(0, self.upper_degree):
+            x_poly[:, d * width:(self.upper_degree + 1) * width] = (x ** (d + 1))
 
-        return self.linear_model.predict(x_poly, theta)
+        return predict(x_poly, theta)
 
-if __name__ == "__main__":
+    def simple_polynomial(self, x, y, theta):
+        """
+        Plot the line of best fit predicted by the model on the given data
+        :param x: the features used to test the model       (Dimensions: m x n)
+        :param y: the target data for the model             (Dimensions: m x 1)
+        :param theta: the weights the model predicts with   (Dimensions: n x 1)
+        """
+        # Set up the figure to visualize the data and model
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        # Plot the data points (x, y) for the given data as a scatterplot
+        ax.scatter(x, y, marker='o', c='r')
+
+        # Create the line of best fit using the theta values and predicting over an interval
+        x_deg = np.linspace(x.min(), x.max(), 100)
+        x_deg = np.reshape(x_deg, (x_deg.size, 1))
+        x_fit = self.generate_polynomial(x_deg, self.upper_degree)
+        y_fit = predict(x_fit, theta)
+        plt.plot(x_deg, y_fit)
+
+        plt.show()
+
+
+if __name__ == '__main__':
     data = np.genfromtxt('train_linear.csv', delimiter=',')
 
-    linear = LinearRegression(data[1:, 0:1], data[1:, 1:2])
-    model = PolynomialRegression(linear, degree=3)
-    theta_size = np.size(model.linear_model.x, axis=1)
+    linear = LinearRegression(data[:, 0:1], data[:, 1:2])
 
-    model.theta, cost, grads = model.linear_model.gradient_descent(.0005, np.zeros((theta_size, 1)),
-                                                                   model.linear_model.x, model.linear_model.y,
-                                                                   model.linear_model.cost)
+    model = PolynomialRegression(linear, degree=4)
+    theta_size = np.size(model.linear_model.x, axis=1)
+    model.theta, cost, grads = batch_gradient_descent(.1**3, np.zeros((theta_size, 1)),
+                                                      model.linear_model.x, model.linear_model.y,
+                                                      model.linear_model.cost)
     print(model.theta, cost, grads)
+
+    model.simple_polynomial(data[:, 0:1], model.linear_model.y, model.theta)
